0e130fb0c6ceee3264ca428b19d57a749a248587
twozeros <pak.kiruha@yandex.ru>
2023-11-18 11:44:04 +0300

    KKL-06 Добавить обработку ошибки 404 для задачи

--- service/src/main/java/ru/twozeros/todo/service/exception/TaskNotFoundException.java
+++ service/src/main/java/ru/twozeros/todo/service/exception/TaskNotFoundException.java
@@ -1,7 +1,8 @@
 package ru.twozeros.todo.service.exception;
 
 public class TaskNotFoundException extends RuntimeException {
+
     public TaskNotFoundException(Long id) {
-        super("Could not find employee " + id);
+        super("Could not find task" + id);
     }
 }
